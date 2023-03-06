b3dee268b88b5b4347c9823236fedcc161eaac65
Aman Ruhela <119286883+amanruhela2003@users.noreply.github.com>
2023-03-06 10:54:35 +0530

    Create 5.Factorial using recursion.cpp

---
+++ 2.Functions/5.Factorial using recursion.cpp
@@ -0,0 +1,16 @@
+/*
+ * Complete the function 'factorial' given below
+ * @params
+ *  n -> an integer whose factorial is to be calculated
+ * @return
+ *  The factorial of integer n
+ */
+int factorial(int n) {
+  // Write your code here
+  //Base Case
+  if(n==0)
+  {
+    return 1;
+  }
+  return n*factorial(n-1);
+}
